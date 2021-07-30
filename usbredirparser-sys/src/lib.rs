d00e75649f62822f6df0938eacfc3135dfa7d5d8
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-07-30 19:52:22 +0400

    usbredirparser-sys: bind filter.h

--- usbredirparser-sys/src/lib.rs
+++ usbredirparser-sys/src/lib.rs
@@ -1,4 +1,7 @@
-#[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 
 mod bindgen;
 pub use bindgen::*;
+
+mod filter;
+pub use filter::*;
